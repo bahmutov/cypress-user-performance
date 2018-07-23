2f2f6f130e2f2c529064a61af9ec726e9c510df7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-07-23 16:49:39 +0200

    use overwritten cy.visit

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,24 +1,6 @@
 /// <reference types="cypress" />
 
-// marking events and then measuring how long
-// loading the todo app takes
 it('loads TodoMVC app', () => {
-  cy.visit('/', {
-    onBeforeLoad: (win) => {
-      win.performance.mark('start')
-    },
-    onLoad: (win) => {
-      win.performance.mark('end')
-    }
-  })
+  cy.visit('/')
   cy.get('#new-todo').should('be.visible')
-})
-
-afterEach(() => {
-  cy.window().its('performance')
-    .then(performance => {
-      performance.measure('load', 'start', 'end')
-      const measure = performance.getEntriesByName('load', 'measure')[0]
-      console.log(measure)
-    })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -21,5 +23,31 @@
 // Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
+
+
+// get around https://github.com/cypress-io/cypress/issues/2196
+// and start the mark using event callback
+Cypress.on('window:before:load', (win) => {
+  win.performance.mark('start')
+})
+
 // -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.overwrite("visit", (originalVisit, url, options) => {
+  const opts = Cypress._.merge(options, {
+    onLoad: (win) => {
+      console.log('onLoad')
+      win.performance.mark('end')
+      win.performance.measure('load', 'start', 'end')
+    }
+  })
+  return originalVisit(url, opts)
+})
+
+afterEach(() => {
+  cy.window().its('performance')
+    .then(performance => {
+      // performance.measure('load', 'start', 'end')
+      const measure = performance.getEntriesByName('load', 'measure')[0]
+      console.log(measure)
+    })
+})
